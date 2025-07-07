b3a924a53decce42e11c4e0213ffa917e502694f
Juan Ignacio Nicolet <juanignacionico@gmail.com>
2025-07-07 13:15:11 -0300

    Log e index basicos, eliminacion de parte admin

--- main.js
+++ main.js
@@ -1,42 +1,33 @@
-// Lógica para consultar producto por ID desde el formulario en get.html
+let productoContainer = document.querySelector(".container-productos");
 
-document.addEventListener("DOMContentLoaded", () => {
-    const get_id = document.getElementById("lista-getId");
-    const get_producto = document.getElementById("getProduct-form");
-
-    if(get_producto) {
-        get_producto.addEventListener("submit", async(event) => {
-            event.preventDefault(); // Evitamos envio por defecto
-
-            try {
-                let formData = new FormData(event.target);
-                let data = Object.fromEntries(formData.entries());
-                let idProd = data.idProd.trim(); // Se sacan posibles espacios
-
-                // Validación básica
-                if(!idProd) {
-                    throw new Error("Ingrese un id de producto correcto");
-                }
-                let response = await fetch(`http://localhost:3002/products/${idProd}`);
-
-                // Manejamos el error en una posible respuesta no exitosa
-                if(!response.ok){
-                    throw new Error("Ingrese un id de producto correcto");
-                }
-                let datos = await response.json();
-                let producto = datos.payload;
-
-                let getProductHtml = `
-                    <li class="listado_mostrar">
-                        <img src="${producto.image}" alt="">
-                        <p>Nombre: ${producto.name} Precio: ${producto.price}</p>
-                    </li>
-                `;
-                get_id.innerHTML = getProductHtml;
-            } catch (error){
-                console.log("Error al obtener el producto: ", error);
-                get_id.innerHTML = `${error.message}`;
-            }
-        });
+async function obtenerDatasProductos() {
+        try {
+            const respuesta = await fetch("http://localhost:3000/products"); // Cambia 3000 por 3002
+            const datos = await respuesta.json();
+            mostrarProductos(datos);
+        } catch (error) {
+            console.error("Error al obtener los productos:", error);
+        }
     }
-});
+
+    function mostrarProductos(array) {
+        const listaProductos = array.payload;
+        let htmlProductos = "";
+
+        listaProductos.forEach(producto => {
+            htmlProductos += `
+            <li class="listado_mostrar" type="none">
+                <img src="${producto.image}" alt="">
+                <p>Nombre: ${producto.name}</p>
+                <p>Precio: ${producto.price}</p>
+                <button class="boton-carrito" onclick="agregarCarrito()">Agregar a carrito</button>
+            </li>
+            `;
+        });
+        productoContainer.innerHTML = htmlProductos;
+    }
+
+    // Llamar a la función al cargar la página
+    obtenerDatasProductos();
+
+init();    
